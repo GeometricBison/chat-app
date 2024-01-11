ee815448a9eb6527055b2b0ae36c26a7da8e5f6c
Kevin Xu <kevinxu501@gmail.com>
2024-01-11 15:33:49 -0600

    bcrypt bad

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,4 +1,4 @@
-import bcrypt from "bcrypt";
+// import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 
@@ -16,14 +16,14 @@ export const register = async (req, res) => {
       occupation
     } = req.body;
 
-    const salt = await bcrypt.genSalt();
-    const passwordHash = await bcrypt.hash(password, salt);
+    // const salt = await bcrypt.genSalt();
+    // const passwordHash = await bcrypt.hash(password, salt);
 
     const newUser = new User({
       firstName,
       lastName,
       email,
-      password: passwordHash,
+      password: password,
       picturePath,
       friends,
       location,
@@ -46,7 +46,7 @@ export const login = async (req, res) => {
     const user = await User.findOne({ email: email });
     if (!user) return res.status(400).json({ msg: "User does not exist. " });
 
-    const isMatch = await bcrypt.compare(password, user.password);
+    const isMatch = password === user.password
     if (!isMatch) return res.status(400).json({ msg: "Invalid credentials. " })
 
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
